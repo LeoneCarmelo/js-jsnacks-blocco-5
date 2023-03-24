c3b681570b47c6a4a4517f41af01a956cb3b2141
LeoneCarmelo <carmeloleone@outlook.it>
2023-03-24 12:52:57 +0100

    Fix result

--- JSnack 5/main.js
+++ JSnack 5/main.js
@@ -1,12 +1,13 @@
 /* Scrivi una funzione che accetti tre argomenti:
 un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b" */
-let c = 0
+let c = []
 function myFunction(arr, a, b) {
 if ( a < b && b <= arr.length) {
     for(let i = 0; i < arr.length; i++){
         if ( i != a && i != b && i != 0){
-            console.log(i)
+            c.push(i)
+            console.log(c)
         }
     }
 }
